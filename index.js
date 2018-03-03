6f7c1a666c4d1060bf875e5fd21497cbcd477629
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-03 23:20:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,20 +27,9 @@ class Route {
 
   estimatedTime(optionalArg) {
     if (typeof optionalArg === "undefined") {
-      const vertical = Math.abs(this.horizontal.vertical - this.vertical.vertical)
-      let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-      const start = eastWest.indexOf(this.horizontal.horizontal)
-      const end = eastWest.indexOf(this.vertical.horizontal)
-      const horizontal = Math.abs(start - end)
-
-      return (horizontal + vertical) / 3
+      return this.blocksTravelled() / 3
     } else {
-      const vertical = Math.abs(this.horizontal.vertical - this.vertical.vertical)
-      let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-      const start = eastWest.indexOf(this.horizontal.horizontal)
-      const end = eastWest.indexOf(this.vertical.horizontal)
-      const horizontal = Math.abs(start - end)
-      return (horizontal + vertical) / 2
+      return this.blocksTravelled() / 2
       }
   }
 
